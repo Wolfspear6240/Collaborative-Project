37885938c00c62b9fb26178ba2fb3a99a62b1a2b
David Barton <david.barton309@gmail.com>
2018-12-06 18:13:25 +0000

    Movement
    Added Movement script, still need floor based collisions to fix jump anim

---
+++ Hack_Heist/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform playerPos;
+    public float Speed;
+
+    private Vector3 newPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(playerPos.position.x,playerPos.position.y,transform.position.z);
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        newPos = new Vector3(playerPos.position.x,playerPos.position.y,transform.position.z);
+        transform.position = Vector3.Lerp(transform.position,newPos,Speed*Time.deltaTime);
+    }
+}
--- Hack_Heist/Assets/Scripts/PlayerMovement.cs
+++ Hack_Heist/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float Speed;
+    public float Movement;
+    private float MoveSpeed;
+    public Rigidbody2D rb;
+    public float Jumpforce;
+    public Animator anim;
+    public string Movestate;
+    public bool jumping = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        PlayerMove();
+        Animation();
+       Jump();
+    }
+
+  void PlayerMove()
+  {
+        Movement = Input.GetAxis("Horizontal");
+        MoveSpeed = (Speed*Movement);
+        rb.velocity = new Vector2(MoveSpeed, rb.velocity.y);
+    
+      if(Movement>0){
+          GetComponent<SpriteRenderer>().flipX = false;
+      }
+ 
+     if(Movement<0){
+          GetComponent<SpriteRenderer>().flipX = true;
+      }
+ 
+ 
+ 
+ 
+  }
+
+
+  void  Jump()
+    {
+        if(Input.GetButtonDown("Jump")){
+
+            rb.AddForce(new Vector3(0,Jumpforce,0));
+            jumping = true;
+        }
+        
+        
+    }
+
+    void Animation(){
+        anim.SetFloat("Speed",Mathf.Abs(Movement));
+        anim.SetBool("Jump",jumping);
     }
 }
